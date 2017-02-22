5a490c0b3a20297e7d00785ddae0d51513333035
qinfenglin <qfl2000@gmail.com>
2017-02-22 11:35:28 +0800

    add uuid, string algorithm, scheduler

--- subsets.sh
+++ subsets.sh
@@ -14,13 +14,19 @@ fi
 rm out -fr
 mkdir -p out
 dist/bin/bcp	\
+	boost/type_traits.hpp	\
+	boost/smart_ptr.hpp	\
 	make_shared.hpp	\
 	boost/enable_shared_from_this.hpp	\
 	bind.hpp	\
 	asio.hpp	\
 	thread.hpp	\
+	boost/thread/executors/scheduler.hpp	\
 	unordered_map	\
 	unordered_set	\
+	boost/uuid/uuid_io.hpp	\
+	boost/uuid/uuid_generators.hpp	\
+	boost/algorithm/string.hpp	\
 	out
 
 
